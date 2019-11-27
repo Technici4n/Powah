5f45e5197919a556d350527aaf1e4881c7c77e46
xieaoDev <12222738+xieaoDev@users.noreply.github.com>
2019-11-27 02:57:55 +0100

    Tweaked Solar panels FE/t.

--- src/main/java/zeroneye/powah/block/generator/panel/Panels.java
+++ src/main/java/zeroneye/powah/block/generator/panel/Panels.java
@@ -6,10 +6,10 @@ import zeroneye.powah.block.generator.panel.solar.SolarPanelBlock;
 
 public enum Panels {
     BASIC(Material.ROCK, 3.0F, 1000, 24, 10),
-    HARDENED(Material.ROCK, 30.0F, 10000, 40, 30),
-    BLAZING(Material.IRON, 30.0F, 50000, 100, 70),
-    NIOTIC(Material.IRON, 100.0F, 80000, 400, 200),
-    SPIRITED(Material.IRON, 100.0F, 200000, 800, 500);
+    HARDENED(Material.ROCK, 30.0F, 10000, 60, 40),
+    BLAZING(Material.IRON, 30.0F, 50000, 190, 140),
+    NIOTIC(Material.IRON, 100.0F, 80000, 500, 450),
+    SPIRITED(Material.IRON, 100.0F, 200000, 1800, 1200);
 
     public final Material material;
     public final float resistance;
