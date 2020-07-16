6d6e78ad59d31ff5d21f88a3dab9ce34fda2de24
xieaoDev <12222738+xieaoDev@users.noreply.github.com>
2020-07-16 21:03:32 +0100

    Fixed reactor not accepting solid coolant.

--- src/main/java/owmii/powah/Powah.java
+++ src/main/java/owmii/powah/Powah.java
@@ -1,5 +1,7 @@
 package owmii.powah;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -10,6 +12,9 @@ import owmii.lib.api.IMod;
 import owmii.lib.config.IConfig;
 import owmii.lib.network.Network;
 import owmii.lib.util.FML;
+import owmii.powah.api.PowahAPI;
+import owmii.powah.block.IBlocks;
+import owmii.powah.book.PowahBook;
 import owmii.powah.client.ItemModelProperties;
 import owmii.powah.client.render.entity.EntityRenderer;
 import owmii.powah.client.render.tile.TileRenderer;
@@ -36,6 +41,13 @@ public class Powah implements IMod {
     public void setup(FMLCommonSetupEvent event) {
         Packets.register();
         Features.register();
+
+        PowahAPI.registerSolidCoolant(Blocks.SNOW_BLOCK, 48, -3);
+        PowahAPI.registerSolidCoolant(Items.SNOWBALL, 12, -3);
+        PowahAPI.registerSolidCoolant(Blocks.ICE, 48, -5);
+        PowahAPI.registerSolidCoolant(Blocks.PACKED_ICE, 192, -8);
+        PowahAPI.registerSolidCoolant(Blocks.BLUE_ICE, 568, -17);
+        PowahAPI.registerSolidCoolant(IBlocks.DRY_ICE, 712, -32);
     }
 
     @Override
@@ -45,6 +57,7 @@ public class Powah implements IMod {
             EntityRenderer.register();
             Screens.register();
             ItemModelProperties.register();
+            PowahBook.register();
         }
     }
 
