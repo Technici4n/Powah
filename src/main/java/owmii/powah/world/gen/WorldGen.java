fcf44e2e4decb4da024c67d6755692d2cd255cf9
Owmii <owmii.dev@gmail.com>
2020-11-11 09:24:46 +0100

    Fixed Impossible to disable worldgen, Closes #124.

--- src/main/java/owmii/powah/world/gen/WorldGen.java
+++ src/main/java/owmii/powah/world/gen/WorldGen.java
@@ -19,22 +19,22 @@ import owmii.powah.config.Configs;
 
 @Mod.EventBusSubscriber
 public class WorldGen {
-    public static final ConfiguredFeature<?, ?> URANINITE_ORE_POOR = register("uraninite_ore_poor", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, Blcks.URANINITE_ORE_POOR.getDefaultState(), 5)).func_242733_d(64).func_242728_a().func_242731_b(Configs.GENERAL.poorUraniniteGenChance.get()));
-    public static final ConfiguredFeature<?, ?> URANINITE_ORE = register("uraninite_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, Blcks.URANINITE_ORE.getDefaultState(), 4)).func_242733_d(32).func_242728_a().func_242731_b(Configs.GENERAL.uraniniteGenChance.get()));
-    public static final ConfiguredFeature<?, ?> URANINITE_ORE_DENSE = register("uraninite_ore_dense", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, Blcks.URANINITE_ORE_DENSE.getDefaultState(), 3)).func_242733_d(16).func_242728_a().func_242731_b(Configs.GENERAL.denseUraniniteGenChance.get()));
-    public static final ConfiguredFeature<?, ?> DRY_ICE = register("dry_ice", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, Blcks.DRY_ICE.getDefaultState(), 17)).func_242733_d(64).func_242728_a().func_242731_b(Configs.GENERAL.dryIceGenChance.get()));
-
-
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void gen(BiomeLoadingEvent event) {
-        BiomeGenerationSettingsBuilder generation = event.getGeneration();
-        generation.func_242513_a(GenerationStage.Decoration.UNDERGROUND_ORES, URANINITE_ORE_POOR);
-        generation.func_242513_a(GenerationStage.Decoration.UNDERGROUND_ORES, URANINITE_ORE);
-        generation.func_242513_a(GenerationStage.Decoration.UNDERGROUND_ORES, URANINITE_ORE_DENSE);
-        if (event.getCategory().equals(Biome.Category.TAIGA)
-                || event.getCategory().equals(Biome.Category.ICY)
-                || event.getCategory().equals(Biome.Category.EXTREME_HILLS)) {
-            generation.func_242513_a(GenerationStage.Decoration.UNDERGROUND_ORES, DRY_ICE);
+        if (Configs.GENERAL.oreGen.get()) {
+            ConfiguredFeature<?, ?> ore_poor = register("uraninite_ore_poor", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, Blcks.URANINITE_ORE_POOR.getDefaultState(), 5)).func_242733_d(64).func_242728_a().func_242731_b(Configs.GENERAL.poorUraniniteGenChance.get()));
+            ConfiguredFeature<?, ?> ore = register("uraninite_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, Blcks.URANINITE_ORE.getDefaultState(), 4)).func_242733_d(32).func_242728_a().func_242731_b(Configs.GENERAL.uraniniteGenChance.get()));
+            ConfiguredFeature<?, ?> ore_dense = register("uraninite_ore_dense", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, Blcks.URANINITE_ORE_DENSE.getDefaultState(), 3)).func_242733_d(16).func_242728_a().func_242731_b(Configs.GENERAL.denseUraniniteGenChance.get()));
+            ConfiguredFeature<?, ?> dry_ice = register("dry_ice", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, Blcks.DRY_ICE.getDefaultState(), 17)).func_242733_d(64).func_242728_a().func_242731_b(Configs.GENERAL.dryIceGenChance.get()));
+            BiomeGenerationSettingsBuilder generation = event.getGeneration();
+            generation.func_242513_a(GenerationStage.Decoration.UNDERGROUND_ORES, ore_poor);
+            generation.func_242513_a(GenerationStage.Decoration.UNDERGROUND_ORES, ore);
+            generation.func_242513_a(GenerationStage.Decoration.UNDERGROUND_ORES, ore_dense);
+            if (event.getCategory().equals(Biome.Category.TAIGA)
+                    || event.getCategory().equals(Biome.Category.ICY)
+                    || event.getCategory().equals(Biome.Category.EXTREME_HILLS)) {
+                generation.func_242513_a(GenerationStage.Decoration.UNDERGROUND_ORES, dry_ice);
+            }
         }
     }
 
