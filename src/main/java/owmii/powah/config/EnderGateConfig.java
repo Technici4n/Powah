528b8a35487c0239f3b90e4f622cd99bf94aa59e
Owmii <owmii.dev@gmail.com>
2021-01-08 12:54:39 +0100

    Fixed Ender Cell/Ender Gate channel missmatch, closes #131.

--- src/main/java/owmii/powah/config/EnderGateConfig.java
+++ src/main/java/owmii/powah/config/EnderGateConfig.java
@@ -7,7 +7,7 @@ public class EnderGateConfig extends EnderCellConfig {
         super(builder,
                 new long[]{0, 0, 0, 0, 0, 0, 0},
                 new long[]{100, 1400, 5000, 18000, 40000, 90000, 200000},
-                new int[]{1, 2, 4, 6, 9, 12, 16}
+                new int[]{1, 2, 3, 5, 7, 9, 12}
         );
     }
 }
