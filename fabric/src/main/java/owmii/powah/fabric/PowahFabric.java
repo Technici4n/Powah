0d2cf76e20dce66b746f76efd5690ab70bd77142
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-04-11 08:42:54 -0700

    Implement wrench removal on Forge

--- fabric/src/main/java/owmii/powah/fabric/PowahFabric.java
+++ fabric/src/main/java/owmii/powah/fabric/PowahFabric.java
@@ -3,20 +3,10 @@ package owmii.powah.fabric;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.SharedConstants;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.phys.BlockHitResult;
-import org.jetbrains.annotations.Nullable;
-import owmii.powah.EnvHandler;
 import owmii.powah.Powah;
-import owmii.powah.block.energizing.EnergizingOrbBlock;
-import owmii.powah.lib.block.AbstractEnergyBlock;
 import owmii.powah.lib.item.ItemBase;
+import owmii.powah.lib.util.Wrench;
 
 public class PowahFabric implements ModInitializer {
 	@Override
@@ -30,7 +20,7 @@ public class PowahFabric implements ModInitializer {
 		SharedConstants.CHECK_DATA_FIXER_SCHEMA = checkDataFixer;
 
 		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
-			InteractionResult removeWithWrench = removeWithWrench(player, world, hand, hitResult);
+			InteractionResult removeWithWrench = Wrench.removeWithWrench(player, world, hand, hitResult);
 			if (removeWithWrench != null) {
 				return removeWithWrench;
 			}
@@ -41,27 +31,5 @@ public class PowahFabric implements ModInitializer {
 			}
 			return InteractionResult.PASS;
 		});
-	}
-
-	private static @Nullable InteractionResult removeWithWrench(Player player, Level world, InteractionHand hand, BlockHitResult hitResult) {
-		if (player.isSpectator() || !player.isShiftKeyDown() || !world.mayInteract(player, hitResult.getBlockPos())
-				|| !EnvHandler.INSTANCE.isWrench(player.getItemInHand(hand))) {
-			return null;
-		}
-		var pos = hitResult.getBlockPos();
-		var state = world.getBlockState(pos);
-		if (state.getBlock() instanceof AbstractEnergyBlock<?,?> || state.getBlock() instanceof EnergizingOrbBlock) {
-			var entity = world.getBlockEntity(pos);
-			world.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
-			if (!player.isCreative()) {
-				Block.dropResources(state, world, pos, entity);
-			}
-			// Play a cool sound
-			var group = state.getSoundType();
-			world.playSound(player, pos, group.getBreakSound(), SoundSource.BLOCKS, (group.getVolume() + 1.0F) / 2.0F,
-					group.getPitch() * 0.8F);
-			return InteractionResult.sidedSuccess(world.isClientSide);
-		}
-		return null;
 	}
 }
--- forge/src/main/java/owmii/powah/forge/PowahForge.java
+++ forge/src/main/java/owmii/powah/forge/PowahForge.java
@@ -1,6 +1,9 @@
 package owmii.powah.forge;
 
 import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -8,6 +11,7 @@ import net.minecraftforge.fml.loading.FMLEnvironment;
 import owmii.powah.Powah;
 import owmii.powah.forge.compat.curios.CuriosCompat;
 import owmii.powah.forge.data.DataEvents;
+import owmii.powah.lib.util.Wrench;
 
 @Mod(Powah.MOD_ID)
 public class PowahForge {
@@ -17,6 +21,16 @@ public class PowahForge {
 
 		Powah.init();
 		modEventBus.addListener(DataEvents::gatherData);
+		MinecraftForge.EVENT_BUS.addListener((PlayerInteractEvent.RightClickBlock event) -> {
+			if (event.getUseBlock() == Event.Result.DENY) {
+				return;
+			}
+			var result = Wrench.removeWithWrench(event.getEntity(), event.getLevel(), event.getHand(), event.getHitVec());
+			if (result != null) {
+				event.setCanceled(true);
+				event.setCancellationResult(result);
+			}
+		});
 
 		if (FMLEnvironment.dist.isClient()) {
 			try {
