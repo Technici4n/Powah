c62cc21cfa1809ad24e97b970c8626fa1d530c63
Sebastian Hartte <shartte@users.noreply.github.com>
2023-12-05 00:41:05 +0100

    Merged common and neoforge projects

---
+++ src/main/java/owmii/powah/forge/ChargeableItemsEvent.java
@@ -0,0 +1,28 @@
+package owmii.powah.forge;
+
+import java.util.ArrayList;
+import java.util.List;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.Event;
+
+/**
+ * Fired when gathering items to charge in a player.
+ * You can add stacks to the list to make them chargeable by Powah.
+ */
+public class ChargeableItemsEvent extends Event {
+    private final Player player;
+    private final List<ItemStack> items = new ArrayList<>();
+
+    public ChargeableItemsEvent(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public List<ItemStack> getItems() {
+        return items;
+    }
+}
