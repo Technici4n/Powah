9b38f9024f991002c4df736e5623737b816c3d63
Sebastian Hartte <shartte@users.noreply.github.com>
2023-12-27 23:38:34 +0100

    Change how buckets are found for fluids.

--- src/main/java/owmii/powah/compat/common/FluidCoolant.java
+++ src/main/java/owmii/powah/compat/common/FluidCoolant.java
@@ -1,8 +1,8 @@
 package owmii.powah.compat.common;
 
-import com.google.common.collect.HashMultimap;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -17,19 +17,18 @@ public record FluidCoolant(ResourceLocation id, Fluid fluid, Set<BucketItem> buc
     public static Collection<FluidCoolant> getAll() {
         List<FluidCoolant> result = new ArrayList<>();
 
-        var buckets = HashMultimap.<Fluid, BucketItem>create();
-        BuiltInRegistries.ITEM.forEach(item -> {
-            if (item instanceof BucketItem bucketItem) {
-                buckets.put(bucketItem.getFluid(), bucketItem);
-            }
-        });
-
         for (var entry : PowahAPI.COOLANT_FLUIDS.entrySet()) {
             var id = entry.getKey();
             var heat = entry.getValue();
             var fluid = BuiltInRegistries.FLUID.get(id);
             if (fluid != Fluids.EMPTY) {
-                result.add(new FluidCoolant(Powah.id("fluid_coolant/" + id.getNamespace() + "/" + id.getPath()), fluid, buckets.get(fluid), heat));
+                var buckets = new HashSet<BucketItem>();
+                var bucket = fluid.getBucket();
+                if (bucket instanceof BucketItem bucketItem && bucketItem.getFluid() == fluid) {
+                    buckets.add(bucketItem);
+                }
+
+                result.add(new FluidCoolant(Powah.id("fluid_coolant/" + id.getNamespace() + "/" + id.getPath()), fluid, buckets, heat));
             }
         }
 
--- src/main/java/owmii/powah/compat/common/MagmatorFuel.java
+++ src/main/java/owmii/powah/compat/common/MagmatorFuel.java
@@ -1,8 +1,8 @@
 package owmii.powah.compat.common;
 
-import com.google.common.collect.HashMultimap;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -17,19 +17,18 @@ public record MagmatorFuel(ResourceLocation id, Fluid fluid, Set<BucketItem> buc
     public static Collection<MagmatorFuel> getAll() {
         List<MagmatorFuel> result = new ArrayList<>();
 
-        var buckets = HashMultimap.<Fluid, BucketItem>create();
-        BuiltInRegistries.ITEM.forEach(item -> {
-            if (item instanceof BucketItem bucketItem) {
-                buckets.put(bucketItem.getFluid(), bucketItem);
-            }
-        });
-
         for (var entry : PowahAPI.MAGMATIC_FLUIDS.entrySet()) {
             var id = entry.getKey();
             var heat = entry.getValue();
             var fluid = BuiltInRegistries.FLUID.get(id);
             if (fluid != Fluids.EMPTY) {
-                result.add(new MagmatorFuel(Powah.id("magmator_fuel/" + id.getNamespace() + "/" + id.getPath()), fluid, buckets.get(fluid), heat));
+                var buckets = new HashSet<BucketItem>();
+                var bucket = fluid.getBucket();
+                if (bucket instanceof BucketItem bucketItem && bucketItem.getFluid() == fluid) {
+                    buckets.add(bucketItem);
+                }
+
+                result.add(new MagmatorFuel(Powah.id("magmator_fuel/" + id.getNamespace() + "/" + id.getPath()), fluid, buckets, heat));
             }
         }
 
