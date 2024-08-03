3f854990bf27917b4a3a60069667088b3cc625d3
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-08-03 11:00:32 +0200

    Update tags: add tags for variant blocks, and make block and item tags match

--- src/main/java/owmii/powah/data/ITags.java
+++ src/main/java/owmii/powah/data/ITags.java
@@ -6,6 +6,7 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import owmii.powah.Powah;
 
 public class ITags {
     public static class Blocks {
@@ -16,10 +17,31 @@ public class ITags {
         public static final TagKey<Block> ICES_BLUE = tag("ices/blue");
         public static final TagKey<Block> ICES_DRY = tag("ices/dry");
         public static final TagKey<Block> URANINITE_ORE = tag("ores/uraninite");
+        public static final TagKey<Block> URANINITE_ORE_REGULAR = tag("ores/uraninite_regular");
+        public static final TagKey<Block> URANINITE_ORE_DENSE = tag("ores/uraninite_dense");
+        public static final TagKey<Block> URANINITE_ORE_POOR = tag("ores/uraninite_poor");
         public static final TagKey<Block> URANINITE_BLOCK = tag("storage_blocks/uraninite");
+
+        public static final TagKey<Block> ENERGY_CELLS = powah("energy_cells");
+        public static final TagKey<Block> ENDER_CELLS = powah("ender_cells");
+        public static final TagKey<Block> ENERGY_CABLES = powah("energy_cables");
+        public static final TagKey<Block> ENDER_GATES = powah("ender_gates");
+        public static final TagKey<Block> ENERGIZING_RODS = powah("energizing_rods");
+        public static final TagKey<Block> FURNATORS = powah("furnators");
+        public static final TagKey<Block> MAGMATORS = powah("magmators");
+        public static final TagKey<Block> THERMO_GENERATORS = powah("thermo_generators");
+        public static final TagKey<Block> SOLAR_PANELS = powah("solar_panels");
+        public static final TagKey<Block> REACTORS = powah("reactors");
+        public static final TagKey<Block> PLAYER_TRANSMITTERS = powah("player_transmitters");
+        public static final TagKey<Block> ENERGY_HOPPERS = powah("energy_hoppers");
+        public static final TagKey<Block> ENERGY_DISCHARGERS = powah("energy_dischargers");
 
         private static TagKey<Block> tag(String name) {
             return BlockTags.create(ResourceLocation.fromNamespaceAndPath("c", name));
+        }
+
+        private static TagKey<Block> powah(String name) {
+            return BlockTags.create(Powah.id(name));
         }
     }
 
@@ -39,8 +61,26 @@ public class ITags {
         public static final TagKey<Item> WRENCHES = tag("tools/wrench");
         public static final TagKey<Item> QUARTZ_BLOCKS = tag("storage_blocks/quartz");
 
+        public static final TagKey<Item> ENERGY_CELLS = powah("energy_cells");
+        public static final TagKey<Item> ENDER_CELLS = powah("ender_cells");
+        public static final TagKey<Item> ENERGY_CABLES = powah("energy_cables");
+        public static final TagKey<Item> ENDER_GATES = powah("ender_gates");
+        public static final TagKey<Item> ENERGIZING_RODS = powah("energizing_rods");
+        public static final TagKey<Item> FURNATORS = powah("furnators");
+        public static final TagKey<Item> MAGMATORS = powah("magmators");
+        public static final TagKey<Item> THERMO_GENERATORS = powah("thermo_generators");
+        public static final TagKey<Item> SOLAR_PANELS = powah("solar_panels");
+        public static final TagKey<Item> REACTORS = powah("reactors");
+        public static final TagKey<Item> PLAYER_TRANSMITTERS = powah("player_transmitters");
+        public static final TagKey<Item> ENERGY_HOPPERS = powah("energy_hoppers");
+        public static final TagKey<Item> ENERGY_DISCHARGERS = powah("energy_dischargers");
+
         private static TagKey<Item> tag(String name) {
             return ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", name));
+        }
+
+        private static TagKey<Item> powah(String name) {
+            return ItemTags.create(Powah.id(name));
         }
     }
 }
