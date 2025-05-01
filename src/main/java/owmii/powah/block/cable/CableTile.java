5d98cb39ce1719e98babf3894f44addb5e2b1567
Sebastian Hartte <shartte@users.noreply.github.com>
2025-05-01 11:59:10 +0200

    formatting

--- src/main/java/owmii/powah/block/cable/CableTile.java
+++ src/main/java/owmii/powah/block/cable/CableTile.java
@@ -10,7 +10,6 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.capabilities.BlockCapabilityCache;
 import net.neoforged.neoforge.capabilities.Capabilities;
