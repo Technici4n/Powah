1b82fff34a8c3c67ba2e6f948ee25cb6f0a836fb
Mithi83 <28407460+Mithi83@users.noreply.github.com>
2026-03-27 11:36:50 +0100

    Allow Ender Gates to be waterlogged
    Fixes #221

--- src/main/java/owmii/powah/block/ender/EnderGateBlock.java
+++ src/main/java/owmii/powah/block/ender/EnderGateBlock.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
@@ -16,7 +17,7 @@ import owmii.powah.lib.block.PowahBaseBlockEntity;
 import owmii.powah.lib.block.PowahBaseEnderBlock;
 import owmii.powah.lib.logistics.inventory.BaseMenu;
 
-public class EnderGateBlock extends PowahBaseEnderBlock<EnderGateBlock> {
+public class EnderGateBlock extends PowahBaseEnderBlock<EnderGateBlock> implements SimpleWaterloggedBlock {
     private static final Map<Direction, VoxelShape> SHAPES = Map.of(
             Direction.UP, box(6.0D, 15.5D, 6.0D, 10.0D, 16.0D, 10.0D),
             Direction.DOWN, box(6.0D, 0.0D, 6.0D, 10.0D, 0.5D, 10.0D),
